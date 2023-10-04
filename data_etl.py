f79b8af04f08cc1ee34e591963eded253465446c
Basazin <belhub@uw.edu>
2023-10-04 15:32:53 -0400

    clean starter

--- data_etl.py
+++ data_etl.py
@@ -14,21 +14,87 @@ def load_file(file):
     '''loads csv to pd dataframe'''
     return pd.read_csv(file)
 
-def consolidate_data(df1, df2, key=None, left_index=False, right_index=False):
-    '''perform inner join to return only records that are present in both dataframes'''
-    return pd.merge(left=df1, right=df2, how='inner', on=key, left_index=left_index, right_index=right_index)
+# def consolidate_data(df1, df2, key=None, left_index=False, right_index=False):
+#     '''perform inner join to return only records that are present in both dataframes'''
+#     return pd.merge(left=df1, right=df2, how='inner', on=key, left_index=left_index, right_index=right_index)
 
-def clean_data(raw_df):
-    '''remove rows that contain salary <= 0 or duplicate job IDs'''
-    clean_df = raw_df.drop_duplicates(subset='jobId')
-    clean_df = clean_df[clean_df.salary>0]
-    return clean_df
+def inner_join_dataframes(df1, df2, join_key):
+    """
+    Perform an inner join on two DataFrames and return only the records that are present in both DataFrames.
 
-def one_hot_encode_feature_df(df, cat_vars=None, num_vars=None):
-    '''performs one-hot encoding on all categorical variables and combines result with continous variables'''
-    cat_df = pd.get_dummies(df[cat_vars])
-    num_df = df[num_vars].apply(pd.to_numeric)
-    return pd.concat([cat_df, num_df], axis=1)#,ignore_index=False)
+    Parameters:
+    df1 (pd.DataFrame): The first DataFrame.
+    df2 (pd.DataFrame): The second DataFrame.
+    join_key (str): The name of the column to perform the inner join on.
+
+    Returns:
+    pd.DataFrame: A new DataFrame containing only the records present in both input DataFrames.
+    """
+
+    # Perform an inner join on the specified key
+    result_df = pd.merge(df1, df2, on=join_key, how='inner')
+    return result_df
+
+
+# def clean_data(raw_df):
+#     '''remove rows that contain salary <= 0 or duplicate job IDs'''
+#     clean_df = raw_df.drop_duplicates(subset='jobId')
+#     clean_df = clean_df[clean_df.salary>0]
+#     return clean_df
+
+
+def remove_rows_with_salary_and_duplicates(df):
+    """
+    Remove rows from a DataFrame that have a salary less than or equal to 0 or duplicate job IDs.
+
+    Parameters:
+    df (pd.DataFrame): The input DataFrame.
+
+    Returns:
+    pd.DataFrame: A new DataFrame with rows removed based on the specified conditions.
+    """
+
+    # Remove rows with salary less than or equal to 0
+    df = df[df['salary'] > 0]
+
+    # Remove rows with duplicate job IDs
+    df = df.drop_duplicates(subset='jobId', keep='first')
+
+    return df
+
+
+# def one_hot_encode_feature_df(df, cat_vars=None, num_vars=None):
+#     '''performs one-hot encoding on all categorical variables and combines result with continous variables'''
+#     cat_df = pd.get_dummies(df[cat_vars])
+#     num_df = df[num_vars].apply(pd.to_numeric)
+#     return pd.concat([cat_df, num_df], axis=1)#,ignore_index=False)
+
+
+def one_hot_encode_and_combine(df, categorical_columns):
+    """
+    Perform one-hot encoding on categorical variables and combine the results with continuous variables.
+
+    Parameters:
+    df (pd.DataFrame): The input DataFrame containing both categorical and continuous variables.
+    categorical_columns (list): List of column names that are categorical variables.
+
+    Returns:
+    pd.DataFrame: A new DataFrame with one-hot encoded categorical variables concatenated with continuous variables.
+    """
+
+    # Ensure that the categorical_columns exist in the DataFrame
+    missing_cols = set(categorical_columns) - set(df.columns)
+    if missing_cols:
+        raise ValueError(f"Columns {missing_cols} not found in the DataFrame.")
+
+    # Create a new DataFrame for one-hot encoded categorical variables
+    encoded_categorical = pd.get_dummies(df[categorical_columns], prefix=categorical_columns)
+
+    # Concatenate the one-hot encoded categorical DataFrame with the original continuous DataFrame
+    combined_df = pd.concat([encoded_categorical, df.drop(categorical_columns, axis=1)], axis=1)
+
+    return combined_df
+
 
 def get_target_df(df, target):
     '''returns target dataframe'''
--- engine.py
+++ engine.py
@@ -1,5 +1,6 @@
 #
-from data_etl import load_file, clean_data, consolidate_data,  one_hot_encode_feature_df, get_target_df
+from data_etl import load_file, remove_rows_with_salary_and_duplicates
+from data_etl import  inner_join_dataframes,  one_hot_encode_and_combine, get_target_df
 from sklearn.utils import shuffle
 # define inputs
 train_feature_file = 'data/train_features.csv'
@@ -18,15 +19,15 @@ target_df = load_file(train_target_file)
 test_df = load_file(test_feature_file)
 
 #consolidate training data
-raw_train_df = consolidate_data(feature_df, target_df, key='jobId')
+raw_train_df = inner_join_dataframes(feature_df, target_df, 'jobId')
 
 #clean, shuffle, and reindex training data -- shuffling improves cross-validation accuracy
-clean_train_df = shuffle(clean_data(raw_train_df)).reset_index()
+clean_train_df = shuffle(remove_rows_with_salary_and_duplicates(raw_train_df)).reset_index()
 
 #encode categorical data and get final feature dfs
 print("Encoding data")
-feature_df = one_hot_encode_feature_df(clean_train_df, cat_vars=categorical_vars, num_vars=numeric_vars)
-test_df = one_hot_encode_feature_df(test_df, cat_vars=categorical_vars, num_vars=numeric_vars)
+feature_df = one_hot_encode_and_combine(clean_train_df, categorical_vars)
+test_df = one_hot_encode_and_combine(test_df, categorical_vars)
 
 #get target df
 target_df = get_target_df(clean_train_df, target_var)
